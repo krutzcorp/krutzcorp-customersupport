945a75e957cb8744c3b261c3fd5d0904574b3c7d
Corban Mailloux <github@corb.co>
2017-03-27 15:23:54 -0400

    Use sales' URL

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 DATABASE_URI = 'sqlite:///customer-support.db'
 
 HR_URL = 'http://hr.krutz.site'
-SALES_URL = 'http://sales.krutz.site'
+# SALES_URL = 'http://sales.krutz.site'
+SALES_URL = 'http://vm343c.se.rit.edu/api'
