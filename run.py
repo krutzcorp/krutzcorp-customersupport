3fd8b54be327406dfeede97bd3001e7505ee3f4e
paradime <brw4824@rit.edu>
2017-03-27 15:50:22 -0400

    made port dynamic

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from customersupport import app
 import os
 
 if('ENV' in os.environ and os.environ['ENV'] == 'production'):
-    app.run('129.21.208.178', port=80, debug=False)
+    app.run('0.0.0.0', port=80, debug=False)
 else:
     app.run(debug=True)
