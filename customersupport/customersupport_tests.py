f686c7d09f5d521fda5b4b17933f7bd89ed9c19b
eric <eyoon13@punahou.edu>
2017-03-27 22:52:31 -0400

    made new tests for wrappers and correctness of data being recieved. Errors and failures occur since some of the APIs 404. Other are issues on our end(passing json data into models).

--- customersupport/customersupport_tests.py
+++ customersupport/customersupport_tests.py
@@ -1,53 +1,169 @@
 import unittest
-from customersupport.models import Employee
-from customersupport.controllers import session
-from customersupport.wrappers import hr
+import requests
+import requests_mock
+from config import SALES_URL, HR_URL
 from customersupport.wrappers import sales
-from customersupport import session, customersearch, ordersearch, ticket
+from customersupport.models import Employee
+from customersupport.wrappers import hr
+from customersupport.wrappers import hr as hr_wrapper
+
+
+class Stubber:
+    @staticmethod
+    def stubMock(m,json=None):
+        if json is None:
+            return m.get(HR_URL, json={'hello': 'world'})
+        else:
+            return m.get(HR_URL, json=json)
+            # This json can be replaced with a real response,
+            # or the response can be passed into the function
+class StubberSalesCustomer:
+    def stubMock(m, json=None):
+        json = {'Hola': 'hola'}
+        if json is None:
+            return m.get(SALES_URL, json={'Hola': 'Hola'})
+        else:
+            return m.get('http://vm343c.se.rit.edu/api/customer?firstName=Joe',json=json)
+class StubberSalesOrder:
+    def stubMock(m, json=None):
+        text = 'http://vm343c.se.rit.edu/api/order/search?address=John+doe+1111+street+Rochester+NY+14568&billingAddress=False&paymentInfo=False&shippingInfo=False&customerInfo=False&items=False'
+        json ={"orders":[]}
+        if json is None:
+            return m.get(SALES_URL, json={'Hola': 'Hola'})
+        else:
+            return m.get(text,json=json)
+class StubberSalesOrderInfo:
+    def stubMock(m, json=None):
+        text = 'http://vm343c.se.rit.edu/api/order?orderId=1&paymentInfo=False&shippingInfo=False&customerInfo=False&items=False'
+        json = {"orders":[{"id":1,"customerId":1,"repId":99,"cost":200,"orderDate":"2017-03-01T20:51:26.905Z","isPaid":False,"taxPercentage":8}]}
+        if json is None:
+            return m.get(SALES_URL, json={'Hola': 'Hola'})
+        else:
+            return m.get(text,json=json)
+
+class StubberSalesReturn:
+    def stubMock(m, json=None):
+        json={'Hola': 'Hola'}
+        if json is None:
+            return m.get(SALES_URL, json={'Hola': 'Hola'})
+        else:
+            return m.get('http://vm343c.se.rit.edu/api/return',json=json)
 
 
 class TestCase(unittest.TestCase):
+
     #def setUp(self):
 
     #def tearDown(self):
-    def routes_tests(self):
-        # test HR route
-        test_session_user = Employee(session.get_employee_stubbed())
-        test_customers = customersearch()
-        self.assertEqual(test_session_user.name, "Corbin Mallioux")
-        assert len(test_customers) == 2
-        assert test_session_user.name == "Corbin Mallioux"
-        assert test_session_user.id == "1"
 
+#test if server is alive
+    '''def test_sales_response(self):
+        test = SALES_URL + '/customer'
+        response = requests.get(test)
+        assert (response.ok)
+    def test_sales_response2(self):
+        test = SALES_URL + '/return'
+        response = requests.get('http://vm343c.se.rit.edu/api/return')
+        assert (response.ok)
+    def test_sales_response3(self):
+        test = SALES_URL + '/search'
+        response = requests.get('http://vm343c.se.rit.edu/api/search')
+        assert(response.ok)
+    def test_sales_response4(self):
+        test = SALES_URL + '/order'
+        response = requests.get('http://vm343c.se.rit.edu/api/order')
+        assert(response.ok)
+    def test_hr_response1(self):
+        test = HR_URL + '/customer'
+        response = requests.get(test)
+        assert(response.ok)'''
+
+#test if wrapper is called
     def test_HR_API(self):
-        #should return None since API we're connecting too isn't implemented yet.
-        self.assertEqual(hr.get_employee(1,False), None)
+        # should return None since API isn't returning data yet
+        self.assertEqual(isinstance(hr.get_employee(1, False),Employee),True)
 
-    def test_Sales_search_cust(self):
-        # should return None since API we're connecting too isn't implemented yet.
-            info = ["0","John","Smith"]
-            sales_search = sales.search_customer(info[0],info[1],info[2])
-            self.assertEqual(sales_search,None)
+#test if hr wrapper is working
+    def testHRMockCalled(self):
+        with requests_mock.mock() as m:
+            Stubber.stubMock(m)
+            hr_wrapper.get_employee(1)
+            actual = m.called
+            expected = True
+            assert actual == expected, "actual: " + str(actual.json())
 
+#test if response of hr wrapper is correct
+    def testHRMockReturnsCorrectResponse(self):
+        with requests_mock.mock() as m:
+            Stubber.stubMock(m)
+            actual = hr.get_employee(1)
+            expected = {"employee_id": 1, "name": "Corban Mailloux"}
+            assert actual.json() == expected, "actual: " + str(actual.json())
+
+#tests for Sales Customer
+    def test_sales_customer(self):
+        with requests_mock.mock() as m:
+            StubberSalesCustomer.stubMock(m)
+            actual = sales.search_customer("Joe")
+            expected = {"firstName": "Joe"}
+            assert actual.json() == expected, "actual:" + str(actual.json())
+
+    def test_sales_customer_called(self):
+        with requests_mock.mock() as m:
+            StubberSalesCustomer.stubMock(m)
+            sales.search_customer("Joe")
+            actual = m.called
+            expected = True
+            assert actual == expected, "actual: " + str(actual.json())
+
+#tests for Sales Refund
     def test_sales_refund(self):
-        # should return None since API we're connecting too isn't implemented yet.
-            info = [True, 24, [20,21]]
-            sales_refund = sales.initiate_refund(info[0],info[1],info[2])
-            self.assertEqual(sales_refund,None)
+        with requests_mock.mock() as m:
+            StubberSalesReturn.stubMock(m)
+            actual = sales.initiate_refund(True,1,1)
+            expected = {}
+            assert actual.json() == expected, "actual:" + str(actual.json())
 
-    def test_sales_getorders(self):
-        # should return None since API we're connecting too isn't implemented yet.
-            info = ["address",True,1,True,True,True,True]
-            sales_grab_orders = sales.get_orders(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
-            self.assertEqual(sales_grab_orders,None)
+    def test_sales_refund_called(self):
+        with requests_mock.mock() as m:
+            StubberSalesReturn.stubMock(m)
+            sales.initiate_refund(True,1,1)
+            actual = m.called
+            expected = True
+            assert actual == expected, "actual: " + str(actual.json())
 
+#tests for Sales order lookup
+    def test_sales_orders(self):
+        with requests_mock.mock() as m:
+            StubberSalesOrder.stubMock(m)
+            actual = sales.get_orders("John doe 1111 street Rochester NY 14568")
+            expected = {"orders":[]}
+            assert actual == expected
+
+    def test_sales_orders_called(self):
+        with requests_mock.mock() as m:
+            StubberSalesOrder.stubMock(m)
+            sales.get_orders("John doe 1111 street Rochester NY 14568")
+            actual = m.called
+            expected = True
+            assert actual == expected, "actual: " + str(actual.json())
+
+#tests for Sales order information
     def test_sales_orderinfo(self):
-        # should return None since API we're connecting too isn't implemented yet.
-            info = [1, True, True, True, True]
-            sales_order_info = sales.get_order_info(info[0], info[1], info[2], info[3], info[4])
-            self.assertEqual(sales_order_info, None)
+        with requests_mock.mock() as m:
+            StubberSalesOrderInfo.stubMock(m)
+            actual = sales.get_order_info(1)
+            expected = {}
+            assert actual.json() == expected, "actual:" + str(actual.json())
 
-    #def test_status(self):
+    def test_sales_orderinfo_called(self):
+        with requests_mock.mock() as m:
+            StubberSalesOrderInfo.stubMock(m)
+            sales.get_order_info(1)
+            actual = m.call
+            expected = True
+            assert actual == expected, "actual: " + str(actual.json())
+
 
 if __name__ == '__main__':
     unittest.main()
