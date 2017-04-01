4430e23486e9a73feb5bbe4716365d047fafa8f7
Corban Mailloux <github@corb.co>
2017-04-01 11:50:58 -0400

    Extract common HTML to a template.

---
+++ customersupport/static/search-customer.js
@@ -0,0 +1,15 @@
+$(document).ready(
+    $("#search").click(function () {
+        $.get("/api/customer/search", $('#searchCustomerForm').serializeArray())
+            .done(function (data) {
+                $("#matchingCustomers").find("option").remove(); // Remove all <option> child tags.
+                $.each(data, function (index, item) { // Iterates through a collection
+                    $("#matchingCustomers").append( // Append an object to the inside of the select box
+                        $("<option></option>") // Yes you can do this.
+                            .text(`${item.first_name} ${item.last_name}, ${item.email}, ${item.phone}`)
+                            .val(item.id)
+                    );
+                });
+            });
+    })
+);
