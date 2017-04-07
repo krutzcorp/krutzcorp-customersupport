ab85677bbc6d8d3b7d8799ad222c077e4c2bae00
Dan Fisher <dfishe33@gmail.com>
2017-04-07 12:08:16 -0400

    API endpoint for ticket searching is working

--- customersupport/controllers/ticket.py
+++ customersupport/controllers/ticket.py
@@ -1,7 +1,12 @@
-from flask import jsonify
-
 from customersupport import app
-from customersupport.wrappers import sales
+from customersupport.models import Ticket, CallLog
+from customersupport.database import db_session
+
+from flask import Flask, request, render_template
+from flask import request
+from flask import render_template
+from flask import jsonify
+from flask import json
 
 
 @app.route('/sales/refund/real')
@@ -26,3 +31,26 @@ def refund_order(mock=False):
 @app.route('/sales/refund/stub')
 def refund_order_stubbed():
     return refund_order(mock=True)
+
+def get_param_from_request_if_not_empty(param_name):
+    """Get a query parameter from the request, or None if the parameter is empty or missing."""
+    value = request.args.get(param_name)
+    if value is not None and value != "":
+        return value
+    else:
+        return None
+
+@app.route('/api/ticket/customer')
+def get_tickets_for_customer():
+    customer_id=get_param_from_request_if_not_empty('customer_id')
+    ticket_id=get_param_from_request_if_not_empty('ticket_id')
+
+    res = []
+    if customer_id is not None:
+        res = Ticket.query.filter_by(customerId=customer_id).all()
+    elif ticket_id is not None:
+        res = Ticket.query.filter_by(id=ticket_id).all()
+    else:
+        res = Ticket.query.all()
+    return jsonify([r.serialize() for r in res])
+
