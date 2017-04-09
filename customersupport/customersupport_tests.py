ca4285c499da5d940d1d4e71cb2a092443f23c16
eric <eyoon13@punahou.edu>
2017-04-09 09:41:45 -0400

    updated tests. changed hr function names to match others.

--- customersupport/customersupport_tests.py
+++ customersupport/customersupport_tests.py
@@ -7,7 +7,7 @@ from customersupport.wrappers import hr, mocked_responses
 
 class TestCase(unittest.TestCase):
 
-    def testHRMockReturnsCorrectResponse(self):
+    def test_hr_employee(self):
         """ test if response of hr wrapper is correct ( hr hasn't given us url yet) """
         actual = hr.get_employee(0)
         expected = mocked_responses.hr_get_employee
@@ -48,29 +48,30 @@ class TestCase(unittest.TestCase):
 
     def test_sales_orders_called(self):
         """ tests for Sales Search Order API is called """
-        actual = sales.get_order_info("John doe 1111 street Rochester NY 14568", True)
-        expected = []
-        assert actual == expected
+        actual = sales.get_order_info("John doe 1111 street Rochester NY 14568", mock=True)
+        final = [actual[0].id,actual[0].order_date]
+        expected = expected = [1000, '2017-03-01T20:51:26.905Z']
+        assert final == expected
 
 
     def test_sales_orderinfo_called(self):
         """tests for Sales order information"""
-        actual = sales.get_order_info(1, True)
-        final = [actual[0].id, actual[0].order_date, actual[0].items]
-        expected = [1, '2017-03-01T20:51:26.905Z', set()]
+        actual = sales.get_order_info(1, mock=True)
+        final = [actual[0].id, actual[0].order_date]
+        expected = [1000, '2017-03-01T20:51:26.905Z']
         assert final == expected
 
     def test_sales_customer_called(self):
         """ Tests to check if search_customer was called """
-        actual = sales.search_customer("Joe", True)
+        actual = sales.search_customer("Joe", mock=True)
         c_first = actual[0].first_name
-        expected = "Joe"
+        expected = "John"
         assert c_first == expected
 
 
-    def testHRMockCalled(self):
+    def test_hr_employee_called(self):
         """ Tests to check if api address is correct and live """
-        actual = hr.get_employee(0, True)
+        actual = hr.get_employee(0, mock=True)
         expected = mocked_responses.hr_get_employee
         assert actual == expected
 
