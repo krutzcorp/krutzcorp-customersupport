48ab9889b5302897424f9e79ae3fc0f1cbcdcbbc
MelissaLaskowski <mxl7583@g.rit.edu>
2017-04-09 22:31:20 -0400

    updated the UI set up the parameters to be called to sales.py

--- customersupport/static/search-order.js
+++ customersupport/static/search-order.js
@@ -2,11 +2,12 @@
  * Created by mxl75 on 4/2/2017.
  */
 
-$(document).ready(
+$(document).ready(function () {
+
     $("#searchOrderButton").click(function () {
         $.get("/api/order/search", $('#searchOrderForm').serializeArray())
             .done(function (data) {
-                $("#matchingOrders").find("option").remove(); // Remove all <option> child tags.
+                $("#matchingOrders").find("option").remove();
                 $.each(data, function (index, item) { // Iterates through a collection
                     $("#matchingOrders").append( // Append an object to the inside of the select box
                         $("<option></option>")
@@ -16,4 +17,10 @@ $(document).ready(
                 });
             });
     })
-);
+
+    // When the user selects a order, populate the new-call form and close the modal.
+    $("#selectOrder").click(function () {
+        $("#order").val($("#matchingOrders").val());
+        $('#searchOrderModal').modal('hide')
+    });
+});
