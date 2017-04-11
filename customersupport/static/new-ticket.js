f2904c6096c23d4108d8b9eb69c611d8798eedda
eric <eyoon13@punahou.edu>
2017-04-11 10:52:35 -0400

    1. Added new ticket modal 2. TODOs     2.1. Post to Sales     2.2. Get Orders Pulling in Correctly (almost done).     2.3. Create Ticket Objects to be put into DB

--- customersupport/static/new-ticket.js
+++ customersupport/static/new-ticket.js
@@ -2,11 +2,28 @@ $(document).ready(function () {
 
     // Takes the drop down selection creates a modal with the ticket type and order number.
     $("#createTicket").click(function (){
+
+        // Grab Type of Ticket and Order Number to create new ticket modal
         var e = document.getElementById("ticketType");
         var ticketType = e.options[e.selectedIndex].value;
         var orderID = $('#order').val();
         $(".modal-body #titleForm").val('['+ ticketType + '] Order '+ orderID);
+        var getInfo= [ticketType, OrderID];
 
+        // Search for items within an order to add to the form
+        $.get("/api/orderinfo/real", $('#order').val())
+            .done(function (data) {
+                $("#matchingReturnOrders").find("option").remove();
+                $.each(data, function (index, item) { // Iterates through a collection of
+                    $("#matchingReturnOrders").append( // Append an object to the inside of the select box
+                        $("<option></option>")
+                            .text(`${item.id}, ${item.status}`)
+                            .val(item.id)
+                    );
+                });
+            });
      });
+
+
 
 });
