9b837e2f8095b6a7cc25f93cccb0139c0240b62c
MelissaLaskowski <mxl7583@g.rit.edu>
2017-04-22 10:06:20 -0400

    updated for sales new api calls

--- customersupport/controllers/ordersearch.py
+++ customersupport/controllers/ordersearch.py
@@ -4,6 +4,7 @@ from flask import request
 from customersupport import app
 from customersupport.wrappers import sales
 
+
 def get_param_from_request_if_not_empty(param_name):
     """Get a query parameter from the request, or None if the parameter is empty or missing."""
     value = request.args.get(param_name)
@@ -12,33 +13,45 @@ def get_param_from_request_if_not_empty(param_name):
     else:
         return None
 
+
+def check_address(address):
+    value = request.args.get(address)
+    if value == 'billing':
+        return True
+    else:
+        return False
+
+
 # select which order search to use
 # if order id exists use order search
 # otherwise order info
 @app.route('/api/order/search')
 def select_search():
-
-    # call order search
-    if get_param_from_request_if_not_empty('order_id') is not None:
-        orders = sales.get_order_info(
-            order_id=get_param_from_request_if_not_empty('order_id'),
-            mock=False
-        )
-    # call order info
-    else:
-        orders = sales.get_orders(
-            address=get_param_from_request_if_not_empty('address'),
-            billing_address= True, #check if radio button click,
-            city=get_param_from_request_if_not_empty('city'),
-            state=get_param_from_request_if_not_empty('state'),
-            zipcode=get_param_from_request_if_not_empty('zipcode'),
-            first_name=get_param_from_request_if_not_empty('first_name'),
-            last_name=get_param_from_request_if_not_empty('last_name'),
-            customer_id=get_param_from_request_if_not_empty('customer'),
-            mock=False
-        )
+    orders = sales.get_orders(
+        address=get_param_from_request_if_not_empty('street_address'),
+        billing_address=check_address('address'),  # check if radio button click,
+        city=get_param_from_request_if_not_empty('city'),
+        state=get_param_from_request_if_not_empty('state'),
+        zipcode=get_param_from_request_if_not_empty('zipcode'),
+        first_name=get_param_from_request_if_not_empty('first_name'),
+        last_name=get_param_from_request_if_not_empty('last_name'),
+        customer_id=get_param_from_request_if_not_empty('customer'),
+        mock=False
+    )
 
     # Call the Sales API to get matching . Used by the search-order
+
+    if orders is not None:
+        return jsonify([c.serialize() for c in orders])
+
+
+@app.route('/api/orderid/search')
+def search_order_id():
+
+    orders = sales.get_order_info(
+        order_id=get_param_from_request_if_not_empty('order_id'),
+        mock=False
+    )
 
     if orders is not None:
         return jsonify([c.serialize() for c in orders])
--- customersupport/static/search-order.js
+++ customersupport/static/search-order.js
@@ -1,8 +1,5 @@
-/**
- * Created by mxl75 on 4/2/2017.
- */
-
 $(document).ready(function () {
+
 
     $("#searchOrderButton").click(function () {
         $.get("/api/order/search", $('#searchOrderForm').serializeArray())
@@ -16,7 +13,22 @@ $(document).ready(function () {
                     );
                 });
             });
-    })
+    });
+
+    $("#searchOrderIdButton").click(function () {
+        $.get("/api/orderid/search", $('#searchOrderIdForm').serializeArray())
+            .done(function (data) {
+                $("#matchingOrders").find("option").remove();
+                $.each(data, function (index, item) { // Iterates through a collection
+                    $("#matchingOrders").append( // Append an object to the inside of the select box
+                        $("<option></option>")
+                            .text(`${item.first_name} ${item.last_name}, ${item.zipcode}, ${item.state}, ${item.street_address}, ${item.customer_id}`)
+                            .val(item.id)
+                    );
+                });
+            });
+
+    });
 
     // When the user selects a order, populate the new-call form and close the modal.
     $("#selectOrder").click(function () {
