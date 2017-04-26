5303f6c1a52efb6d66479cfcddc5ada88fc176ab
MelissaLaskowski <mxl7583@g.rit.edu>
2017-04-26 00:09:59 -0400

    updated for the display in the matching orders box

--- customersupport/static/search-order.js
+++ customersupport/static/search-order.js
@@ -8,9 +8,12 @@ $(document).ready(function () {
                 $.each(data, function (index, item) { // Iterates through a collection
                     $("#matchingOrders").append( // Append an object to the inside of the select box
                         $("<option></option>")
-                            .text(`${item.first_name} ${item.last_name}, ${item.zipcode}, ${item.state}, ${item.street_address}, ${item.customer_id}`)
-                            .val(item.id)
-                    );
+                            .text(`Order Id: ${item.id}, Order Date: ${item.orderDate.substring(0,10)}, Phones Purchased: `)
+                            .val(item.id))
+                            $.each(item.items, function (index, phone) {
+                            $("<option></option>")
+                            .text(`Model Id: ${phone.modelId}, Refund Deadline: ${phone.refundDeadline} Replace Deadline: ${phone.replaceDeadline}, Serial Id: ${phone.serialId}`)
+                        })
                 });
             });
     });
@@ -22,8 +25,12 @@ $(document).ready(function () {
                 $.each(data, function (index, item) { // Iterates through a collection
                     $("#matchingOrders").append( // Append an object to the inside of the select box
                         $("<option></option>")
-                            .text(`${item.first_name} ${item.last_name}, ${item.zipcode}, ${item.state}, ${item.street_address}, ${item.customer_id}`)
-                            .val(item.id)
+                            .text(`Order Id: ${item.id}, Order Date: ${item.orderDate.substring(0,10)}, Phones Purchased: `)
+                            .val(item.id),
+                        $.each(item.items, function (index, phone) {
+                            $("<option></option>")
+                            .text(`Model Id: ${phone.modelId}, Refund Deadline: ${phone.refundDeadline} Replace Deadline: ${phone.replaceDeadline}, Serial Id: ${phone.serialId}`)
+                        })
                     );
                 });
             });
