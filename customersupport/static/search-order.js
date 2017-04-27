68446f2c21c62bc3b59186218d7b09df6aaefb64
MelissaLaskowski <mxl7583@g.rit.edu>
2017-04-27 09:53:21 -0400

    updated display of orders

--- customersupport/static/search-order.js
+++ customersupport/static/search-order.js
@@ -6,16 +6,18 @@ $(document).ready(function () {
             .done(function (data) {
                 $("#matchingOrders").find("option").remove();
                 $.each(data, function (index, item) { // Iterates through a collection
+                    var phones = [];
+                    $.each(item.items, function(index, items) {
+                   phones.push(`Model Id: ${items.modelId}, Refund Deadline: ${items.refundDeadline.substring(0,10)} Replace Deadline: ${items.replaceDeadline.substring(0,10)}, Serial Id: ${items.serialId} `);
+                    });
                     $("#matchingOrders").append( // Append an object to the inside of the select box
                         $("<option></option>")
-                            .text(`Order Id: ${item.id}, Order Date: ${item.orderDate.substring(0,10)}, Phones Purchased: `)
-                            .val(item.id))
-                            $.each(item.items, function (index, items) {
-                            $("<option></option>")
-                            .text(`Model Id: ${items.modelId}, Refund Deadline: ${items.refundDeadline.substring(0,10)} Replace Deadline: ${items.replaceDeadline.substring(0,10)}, Serial Id: ${items.serialId}`)
-                        })
+                        .text(`Order Id: ${item.id}, Order Date: ${item.orderDate.substring(0,10)}, Phones Purchased: ${phones}`)
+                        .val(item.id)
+                    )
+
                 });
-            });
+                });
     });
 
     $("#searchOrderIdButton").click(function () {
@@ -23,19 +25,19 @@ $(document).ready(function () {
             .done(function (data) {
                 $("#matchingOrders").find("option").remove();
                 $.each(data, function (index, item) { // Iterates through a collection
+                    var phones = [];
+                    $.each(item.items, function(index, items) {
+                   phones.push(`Model Id: ${items.modelId}, Refund Deadline: ${items.refundDeadline.substring(0,10)} Replace Deadline: ${items.replaceDeadline.substring(0,10)}, Serial Id: ${items.serialId} `);
+                    });
                     $("#matchingOrders").append( // Append an object to the inside of the select box
                         $("<option></option>")
-                            .text(`Order Id: ${item.id}, Order Date: ${item.orderDate.substring(0,10)}, Phones Purchased: `)
-                            .val(item.id),
-                        $.each(item.items, function (index, items) {
-                            $("<option></option>")
-                            .text(`Model Id: ${items.modelId}, Refund Deadline: ${items.refundDeadline.substring(0,10)} Replace Deadline: ${items.replaceDeadline.substring(0,10)}, Serial Id: ${items.serialId}`)
-                        })
-                    );
+                        .text(`Order Id: ${item.id}, Order Date: ${item.orderDate.substring(0,10)}, Phones Purchased: ${phones}`)
+                        .val(item.id)
+                    )
+
+                });
                 });
             });
-
-    });
 
     // When the user selects a order, populate the new-call form and close the modal.
     $("#selectOrder").click(function () {
