b127a679a134c9bb8d9d3545d64328a4bac2d5f9
Bryon Wilkins <paradime@users.noreply.github.com>
2017-04-27 14:29:16 -0400

    add db initialization to deploy script (#25)

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ git pull origin master
 echo "installing requirements"
 source venv/bin/activate
 pip install -r requirements/prod.txt
+echo "initializing db"
+python3 init_db.py
 echo "restarting app"
 sudo service krutzcorp-customersupport restart
 echo "restarting nginx"
